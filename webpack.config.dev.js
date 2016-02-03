32b6b320b3bbc0356bb226d49ef6bb5bb01b4925
Adam Beck <adam@adambeck.me>
2016-02-02 22:32:07 -0500

    Initial commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var precss = require('precss');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'eventsource-polyfill', // necessary for hot reloading with IE
+    'webpack-hot-middleware/client',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['babel'],
+      include: path.join(__dirname, 'src')
+    }, {
+      test: /\.css$/,
+      loaders: ['style', 'css?modules&importLoaders=1', 'postcss'],
+      include: path.join(__dirname, 'src')
+    }]
+  }
+};
